1acefa534a8c0bba07418817625737ec8e72a00a
Nilay Parikh <me@nilayparikh.com>
2019-12-29 23:40:29 +0000

    another update

--- openvpn/start.sh
+++ openvpn/start.sh
@@ -195,5 +195,5 @@ if [[ -n "${LOCAL_NETWORK-}" ]]; then
   fi
 fi
 
-exec /opt/tinyproxy/start.sh &&
+exec /opt/tinyproxy/start.sh & &&
 exec openvpn --script-security 2 --up-delay ${OPENVPN_OPTS} --config "${OPENVPN_CONFIG}"
